bbba1a873d3cb8970c6a419f2994db3ad8b153f2
halilgatgar <halilgatgar@gmail.com>
2021-02-23 00:00:48 +0300

    creating model

---
+++ todo/src/main/java/com/borsa/todo/model/toDo.java
@@ -0,0 +1,35 @@
+package com.borsa.todo.model;
+
+import javax.persistence.Id;
+
+public class toDo {
+
+    String description;
+    int date;
+    @Id
+    int id;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
